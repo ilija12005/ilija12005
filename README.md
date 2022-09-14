35125c315451fe91eccaf5c6252069a28d5cddcd
ilija12005 <113440930+ilija12005@users.noreply.github.com>
2022-09-14 19:08:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-c1snv3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-c1snv3)
---
+++ index.js
@@ -0,0 +1,27 @@
+//let a = Number(prompt((`Vaše godine`);
+//a=a+1;
+//let b = 1;
+//b=a-1;
+//console.log(a);
+//console.log(b);
+//Ovo je neki komentar
+//
+//let a=!!`abc73f`
+//console.log(a);
+//let a=prompt (`Unesite broj`);
+//console.log(a*a);
+//let a=prompt(`Unesite broj`);
+//if (typeof a=="number"){
+ // console.log(a*a);
+//}
+//if (a>=18){
+  console.log("Punoletni ste");
+//}else{
+  //console.log(`Niste punoletni`);
+//}
+let god=Number(promp("Unesi godine"));
+if (Number.isNaN(god){
+  console.log("Niste uneli broj")
+}else{
+  console.log(`Punoletni ste`)
+}
